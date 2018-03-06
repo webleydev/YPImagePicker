da7a734fdd45d31d465c71d4777e4d649f664173
ios_nikitos <agoshkof@gmail.com>
2018-03-06 14:23:14 +0200

    Fix

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -260,9 +260,7 @@ public class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
     
     func updateUI() {
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
-                                                           target: self,
-                                                           action: #selector(close))
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "<", style: .plain, target: self, action: #selector(close))
         navigationItem.leftBarButtonItem?.tintColor = UIColor(red: 81.0/255.0, green: 174.0/255.0, blue: 95.0/255.0, alpha: 1.0)
         
         switch mode {
