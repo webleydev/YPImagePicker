a97742dc024eac88fe26ad056881e07bbceeb5fb
ios_nikitos <agoshkof@gmail.com>
2018-03-06 12:49:15 +0200

    Fix back button color

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -262,9 +262,8 @@ public class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
     func updateUI() {
        
         let backItem = UIBarButtonItem(image: nil, style: .plain, target: self, action:  #selector(close))
-        backItem.tintColor = UIColor(red: 81.0/255.0, green: 174.0/255.0, blue: 95.0/255.0, alpha: 1.0)
-        navigationItem.leftBarButtonItem = backItem
-        navigationItem.leftBarButtonItem?.tintColor = UIColor(r: 38, g: 38, b: 38)
+        navigationItem.backBarButtonItem = backItem
+        navigationItem.backBarButtonItem?.tintColor = UIColor(red: 81.0/255.0, green: 174.0/255.0, blue: 95.0/255.0, alpha: 1.0)
         switch mode {
         case .library:
             setTitleViewWithTitle(aTitle: libraryVC?.title ?? "")
