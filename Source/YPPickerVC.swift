d0b038240bdc986526d3297bedc93a34b438eb4e
ios_nikitos <agoshkof@gmail.com>
2018-03-07 14:08:25 +0200

    Fix

--- Source/YPPickerVC.swift
+++ Source/YPPickerVC.swift
@@ -264,7 +264,6 @@ public class YPPickerVC: YPBottomPager, YPBottomPagerDelegate {
         let backButton = UIButton(type: .custom)
         backButton.frame = CGRect.init(x: 0, y: 0, width: 29, height: 29)
         backButton.setImage(backImage, for: .normal)
-        backButton.backgroundColor = .black
         backButton.setTitle("", for: .normal)
         backButton.setTitleColor(UIColor(red: 81.0/255.0, green: 174.0/255.0, blue: 95.0/255.0, alpha: 1.0), for: .normal) // You can change the TitleColor
         backButton.addTarget(self, action: #selector(close), for: .touchUpInside)
